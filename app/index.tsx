7d4457bceaf54001acd0fc2bd7eb2207e06d6d94
dev-brunosantos <brunosflauta@gmail.com>
2024-06-15 20:59:23 -0300

    Iniciando o Projeto

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bruno Santos</Text>
+      <Text>Criando novo APP</Text>
       <StatusBar style="auto" />
     </View>
   );
