da0d2b2a805d2f45e364de64f39b10c021d52ad3
dev-brunosantos <brunosflauta@gmail.com>
2024-06-17 08:39:55 -0300

    Adicionando arquivo README

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -3,6 +3,7 @@ import { Cabecalho } from '../../src/components/Cabecalho';
 import { DrawerItem, DrawerItemList } from '@react-navigation/drawer';
 import { Text, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { router } from 'expo-router';
 
 export default function DrawerLayout() {
     return (
@@ -11,7 +12,7 @@ export default function DrawerLayout() {
                 <>
                     <Cabecalho />
                     <DrawerItemList {...props} />
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => router.push('../(stack)')}>
                         <Text>Sair</Text>
                     </TouchableOpacity>
                 </>
