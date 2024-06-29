8dfa7b8004330d7605229705cd1aab84a5c5c249
dev-brunosantos <brunosflauta@gmail.com>
2024-06-29 14:04:10 -0300

    Criando telas do MENU PRINCIPAL

---
+++ app/(drawer)/Configuracoes.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Configuracoes() {
+    return(
+        <View>
+            <Text>Tela de CONFIGURAÇÕES</Text>
+        </View>
+    )
+}
