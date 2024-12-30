9f2219a1bd58a8d0f75b0afabc1e3eb33b3b1182
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-30 09:44:47 -0300

    Criando a tela de LOGIN

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -1,16 +1,36 @@
+import { BtnComponent } from "@/components/BtnComponent";
+import { InputComponent } from "@/components/InputComponents";
+import { useState } from "react";
 import { View, Text } from "react-native";
 
 export default function Login() {
-    return(
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    return (
         <View>
             <Text>Tela de Login</Text>
 
             <View>
+                <InputComponent
+                    isPassword={false}
+                    placeholder="Digite seu e-mail"
+                    onChangeText={setEmail}
+                    />
                 
+                <InputComponent
+                    isPassword={true}
+                    placeholder="Digite sua senha"
+                    onChangeText={setSenha}
+                />
             </View>
 
             <View>
-                
+                <BtnComponent 
+                    titulo="Entrar"
+                    onPress={() => alert(`${email} -- ${senha}`)}
+                />
             </View>
         </View>
     )
