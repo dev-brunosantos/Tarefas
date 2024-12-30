56c786c1bb0ca5e52ba999e85ae20a42d2fa43f3
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-30 10:08:47 -0300

    Usando o contexto de LOGIN

--- src/app/Login.tsx
+++ src/app/Login.tsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 import { View, Text } from "react-native";
 import { BtnComponent } from "@/components/BtnComponent";
 import { InputComponent } from "@/components/InputComponents";
+import { useLogin } from "@/contexts/LoginContext";
 
 export default function Login() {
 
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
+
+    const { erro, signIn } = useLogin()
+
+    const realizarLogin = () => {
+        signIn(email, senha)
+    }
 
     return (
         <View>
@@ -17,8 +24,8 @@ export default function Login() {
                     isPassword={false}
                     placeholder="Digite seu e-mail"
                     onChangeText={setEmail}
-                    />
-                
+                />
+
                 <InputComponent
                     isPassword={true}
                     placeholder="Digite sua senha"
@@ -27,10 +34,16 @@ export default function Login() {
             </View>
 
             <View>
-                <BtnComponent 
+                <BtnComponent
                     titulo="Entrar"
-                    onPress={() => alert(`${email} -- ${senha}`)}
+                    onPress={realizarLogin}
                 />
+            </View>
+
+            <View>
+                {erro && (
+                    <Text>Não possui conta? Cadastre-se aqui</Text>
+                )}
             </View>
         </View>
     )
