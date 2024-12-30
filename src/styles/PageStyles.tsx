fd452ef951a43f26c8ff8d1f18e91c67720e6543
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-30 12:22:31 -0300

    Criando o arquivo de estilos gerais do projeto

---
+++ src/styles/PageStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const PagesStyles = StyleSheet.create({
+    page: {
+        flex: 1,
+        padding: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    
+})
