01fc80ef8f40980c74650ce23ca157906f21f894
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-03 14:20:32 -0300

    Testando o contexto de tema

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import { router } from "expo-router";
 import * as Animacao from 'react-native-animatable';
 import { PagesStyles } from "@/styles/PageStyles";
+import { useTema } from "@/hooks/useTema";
 
 export default function App() {
 
@@ -12,10 +13,12 @@ export default function App() {
         }, 2000)
     })
 
+    const { tema } = useTema()
+
     return (
-        <View style={PagesStyles.page}>
+        <View style={[PagesStyles.page, { backgroundColor: tema.background }]}>
             <Animacao.Text
-                style={{ fontSize: 50, textAlign: 'center', fontWeight: 'bold' }}
+                style={{ fontSize: 50, textAlign: 'center', fontWeight: 'bold', color: tema.txt }}
                 animation="fadeIn"
                 delay={1000}
             >
