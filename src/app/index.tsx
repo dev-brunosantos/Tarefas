1da87d62b18d5c4162997bbe02b2c2c4796a5f05
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-03 14:03:04 -0300

    Melhorando o codigo do arquivo inicial

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { View, Text } from "react-native";
 import { router } from "expo-router";
+import * as Animacao from 'react-native-animatable';
+import { PagesStyles } from "@/styles/PageStyles";
 
 export default function App() {
 
@@ -10,9 +12,15 @@ export default function App() {
         }, 2000)
     })
 
-    return(
-        <View>
-            <Text>Minhas Tarefas</Text>
+    return (
+        <View style={PagesStyles.page}>
+            <Animacao.Text
+                style={{ fontSize: 50, textAlign: 'center', fontWeight: 'bold' }}
+                animation="fadeIn"
+                delay={1000}
+            >
+                Minhas Tarefas
+            </Animacao.Text>
         </View>
     )
 }
