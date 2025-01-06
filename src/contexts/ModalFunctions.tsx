63148c76741e484b3ae16bdf842511f139c2e116
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-01-06 14:39:17 -0300

    Criando o contexto responsável por administrar as funções referentes aos Modais

---
+++ src/contexts/ModalFunctions.tsx
@@ -0,0 +1,54 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface ModalProps {
+    abrir: boolean,
+    finalizada: boolean,
+    atualizar: boolean,
+    openModal: () => void;
+    closeModal: () => void;
+    finished: () => void;
+    update: () => void;
+}
+
+const ModalFunctionsContext = createContext<ModalProps | null>(null)
+
+const ModalFunctionsProvider = ({ children }: { children: ReactNode }) => {
+
+    const [abrir, setAbrir] = useState(false)
+    const [finalizada, setFinalizada] = useState(false)
+    const [atualizar, setAtualizar] = useState(false)
+
+    const openModal = () => {
+        setAbrir(true)
+    }
+
+    const closeModal = () => {
+        setAbrir(false)
+        setAtualizar(false)
+    }
+
+    const finished = () => {
+        setFinalizada(!finalizada)
+    }
+
+    const update = () => {
+        setAtualizar(true)
+    }
+
+    return (
+        <ModalFunctionsContext.Provider value={{
+            abrir, atualizar, finalizada,
+            openModal, closeModal, finished, update
+        }}>
+            {children}
+        </ModalFunctionsContext.Provider>
+    )
+}
+
+const useModalContext = () => {
+    const contexto = useContext(ModalFunctionsContext)
+    if(!contexto) throw new Error("Não existe nenhum contexto de Modal")
+    return contexto
+}
+
+export { ModalFunctionsContext, ModalFunctionsProvider, useModalContext }
